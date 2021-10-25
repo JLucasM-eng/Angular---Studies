f93acaca9cdd08091a9bd15000dad6812fd136ba
JLucasM-eng <jlm16051997@gmail.com>
2021-10-25 19:21:17 -0300

    :tada: create a service and models in agendalive

--- agendalive/agendalive/src/app/app.module.ts
+++ agendalive/agendalive/src/app/app.module.ts
@@ -10,7 +10,8 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { LiveListComponent } from './views/home/live-list/live-list.component';
 import {MatTabsModule} from '@angular/material/tabs';
-
+import {MatCardModule} from '@angular/material/card';
+import {HttpClientModule} from '@angular/common/http'
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,13 +19,15 @@ import {MatTabsModule} from '@angular/material/tabs';
     LiveListComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatTabsModule
+    MatTabsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
