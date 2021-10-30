c9e5c414086344d0339d95a027638eddf68dd84e
JLucasM-eng <jlm16051997@gmail.com>
2021-10-30 19:25:01 -0300

    feat: :sparkles: creating a authentication service

--- animal-book/animal-book/src/app/home/login/login.component.ts
+++ animal-book/animal-book/src/app/home/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +12,7 @@ export class LoginComponent implements OnInit {
   usuario:string=''
   senha:string=''
 
-  constructor() { }
+  constructor(private authService:AutenticacaoService, private router:Router) { }
 
   ngOnInit(): void {
   }
@@ -18,6 +20,13 @@ export class LoginComponent implements OnInit {
   login() {
     console.log(this.usuario);
     console.log(this.senha);
+    this.authService.autenticar(this.usuario,this.senha).subscribe(()=>{
+      console.log("Autenticado com sucesso!")
+      this.router.navigate(['animais'])
+    },(error)=>{
+      alert("Usuário ou senha inválido")
+      console.log(error)
+    })  
   }
 
 }
